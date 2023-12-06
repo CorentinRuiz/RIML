15276790b6f98ffdcd4d36ebdf1c24728b42c15a
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2023-12-06 19:42:40 +0100

    On peut parcourir des repo

--- assets/code/search-producers/main.py
+++ assets/code/search-producers/main.py
@@ -18,28 +18,55 @@ import json
 import shutil
 import subprocess
 import re
+import stat
+import time
 import argparse
+from common.utils import BCOLORS
 
 
-def delete_folder(path):
+def delete_folder(project_name):
     """
     Supprimer un dossier
     :param path: chemin du dossier à supprimer
     """
+    path = f'./projects/{project_name}'
+    # Is the error an access error?
+    os.chmod(path, stat.S_IWUSR)
+    for r, _, ff in os.walk(path):
+        for momo in dirs:
+            os.chmod(os.path.join(r, momo), stat.S_IWUSR)
+        for momo in ff:
+            os.chmod(os.path.join(r, momo), stat.S_IWUSR)
+    print(f"{BCOLORS.OKBLUE}Permissions changed{BCOLORS.ENDC}")
     if os.path.exists(path):
-        shutil.rmtree(f'./projects/{project_name}')
-        print(f"Project {project_name} deleted")
+        print(f"{BCOLORS.OKBLUE}Deleting project {project_name}...{BCOLORS.ENDC}")
+        # Delete using absolute path
+        shutil.rmtree(f'{os.getcwd()}\\projects\\{project_name}')
+        print(f"{BCOLORS.OKBLUE}Project {project_name} deleted{BCOLORS.ENDC}")
+    else:
+        print(f"{BCOLORS.WARNING}Project {project_name} does not exist{BCOLORS.ENDC}")
+
 
 def clone_project(project_name):
     """
     Cloner un projet github
     :param project_name: nom du projet à cloner
     """
-    # Cloner le projet
+    # Vérifier si le dossier ./projects existe, sinon le créer
+    if not os.path.exists('./projects'):
+        print(f"{BCOLORS.WARNING}Folder ./projects does not exist{BCOLORS.ENDC}")
+        os.makedirs('./projects')
+        print(f"{BCOLORS.OKBLUE}Folder ./projects created{BCOLORS.ENDC}")
+
+    # Cloner le projet s'il n'existe pas
+    if os.path.exists(f'./projects/{project_name}'):
+        print(f"{BCOLORS.WARNING}Project {project_name} already exists{BCOLORS.ENDC}")
+        return
     os.chdir('./projects')
+    print(f"{BCOLORS.OKBLUE}Cloning project {project_name}...{BCOLORS.ENDC}")
     subprocess.run(['git', 'clone', f'https://github.com/{project}'])
     os.chdir('..')
-    print(f"Project {project_name} cloned")
+    print(f"{BCOLORS.OKBLUE}Project {project_name} cloned{BCOLORS.ENDC}")
 
 
 if __name__ == '__main__':
@@ -48,22 +75,23 @@ if __name__ == '__main__':
     run_id = str(uuid.uuid4())
 
     parser = argparse.ArgumentParser(description='Extract kafka producers from a github project')
-    parser.add_argument('project', metavar='project', type=str, help='github project to analyze')
-    parser.add_argument('output', metavar='output', type=str, help='output file',
+    parser.add_argument('-p', '--project', metavar='project', type=str, help='github project to analyze')
+    parser.add_argument('-o', '--output', metavar='output', type=str, help='output file',
                         default=f'./outputs/output-{run_id}.json')
     args = parser.parse_args()
 
     # Récupérer le projet github passé en paramètre
     project = args.project
-    output = args.output
-    print(f"Analyzing project {project}...")
+    project_name = project.split('/')[1]
+    project_folder = f"./projects/{project_name}"
+    output = args.output if args.output else f'./outputs/{project_name}-{run_id}.json'
+    print(f"{BCOLORS.HEADER}Analyzing project {project}...{BCOLORS.ENDC}")
 
     # Cloner le projet
-    project_name = project.split('/')[-1]
-    clone_project(project_name)
+    clone_project(project)
 
     # Parcourir les fichiers du projet
-    os.chdir(project_name)
+    os.chdir(project_folder)
     producers = []
     for root, dirs, files in os.walk('.'):
         for file in files:
@@ -72,28 +100,40 @@ if __name__ == '__main__':
                 # Lire le fichier
                 with open(os.path.join(root, file), 'r') as f:
                     content = f.read()
+                    # print(f'{content}')
                     # Extraire les connexions à un bus kafka
-                    matches = re.findall(r'new\s+KafkaProducer\(\s*new\s+Properties\(\)\s*\)', content)
-                    if matches:
-                        # Récupérer le nom du bus kafka et du topic
-                        kafka = re.findall(r'bootstrap.servers\s*=\s*"(.*)"', content)
-                        topic = re.findall(r'topic\s*=\s*"(.*)"', content)
-                        if kafka and topic:
-                            # Ajouter la connexion à la liste des connexions
-                            producer = {
-                                "name": project_name,
-                                "kafka": kafka[0],
-                                "topic": topic[0],
-                                "type": "producer"
-                            }
-                            producers.append(producer)
-                            print(f"Producer found in {os.path.join(root, file)}")
+                    matches = re.findall(r'topic', content)
+                    print(f'{content}') if matches else None
+                    # if matches:
+                    #     # Récupérer le nom du bus kafka et du topic
+                    #     kafka = re.findall(r'bootstrap.servers\s*=\s*"(.*)"', content)
+                    #     topic = re.findall(r'topic\s*=\s*"(.*)"', content)
+                    #     if kafka and topic:
+                    #         # Ajouter la connexion à la liste des connexions
+                    #         producer = {
+                    #             "name": project_name,
+                    #             "kafka": kafka[0],
+                    #             "topic": topic[0],
+                    #             "type": "producer"
+                    #         }
+                    #         producers.append(producer)
+                    #         print(f"Producer found in {os.path.join(root, file)}")
     os.chdir('../..')
+    print(f"{BCOLORS.OKGREEN}Project {project_name} analyzed{BCOLORS.ENDC}")
 
     # Supprimer le projet
-    delete_folder(project_name)
+    # delete_folder(project_name)
+
+    # Créer le dossier ./outputs s'il n'existe pas
+    if not os.path.exists('./outputs'):
+        print(f"{BCOLORS.WARNING}Folder ./outputs does not exist{BCOLORS.ENDC}")
+        os.makedirs('./outputs')
+        print(f"{BCOLORS.OKBLUE}Folder ./outputs created{BCOLORS.ENDC}")
 
     # Ecrire les connexions dans un fichier json
+    if len(producers) == 0:
+        print(f"{BCOLORS.WARNING}No producers found{BCOLORS.ENDC}")
+        exit(0)
     with open(output, 'w') as f:
         json.dump(producers, f)
-    print(f"Producers written in {output}")
+        print(f"{BCOLORS.OKGREEN}Producers written in {output}{BCOLORS.ENDC}")
